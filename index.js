bb7cca1a18bdae949f93488ea05c1bac47aae841
Mywork <mywork@Mys-MacBook-Pro.local>
2022-01-20 05:25:50 +0700

    sss

--- index.js
+++ index.js
@@ -909,77 +909,194 @@ const money = [
 
       
 
-
-
 const item = [
 
 {
-          type: "template",
-          altText: "รายละเอียด",
-          template: {
-            type: "carousel",
-            imageAspectRatio: "square",
-            imageSize: "contain",
-            columns: [
-             {
-                thumbnailImageUrl: "https://www.i-pic.info/i/86hK111426.png",
-                imageBackgroundColor: "#030600",
-                title: "\t\t กาชาดจังหวัดตรัง ",
-                text: " โทรศัพท์ 075501095 \n ที่อยู่ 8,ถนนรื่นรมย์\n ตำบลทับเที่ยง",
-                actions: [
-                 
-                    {
-                    type: "uri",
-                    label: "รายละเอียดเพิ่มเติม",
-                   uri: "https://www.facebook.com/pg/trangrc/posts?openExternalBrowser=1"
-          }
-                ]
-              },
- 
+"type" : "flex",
+    "altText" : "รายละเอียด", 
+    "contents" :{
+  "type": "carousel",
+  "contents": [
  {
-                thumbnailImageUrl: "https://www.i-pic.info/i/Kmq9111425.jpg",
-                imageBackgroundColor: "#030600",
-                title: "\t\t สถานบ้านพักคนชรา",
-                text: " โทรศัพท์ 075574679\n ที่อยู่ 113 ม.2 ต.ปากแจ่ม อ.ห้วยยอด",
-                actions: [
-                    {
-                    type: "uri",
-                    label : "รายละเอียดเพิ่มเติม",
-                    uri : "https://www.facebook.com/%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99%E0%B8%AA%E0%B8%87%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%B0%E0%B8%AB%E0%B9%8C%E0%B8%84%E0%B8%99%E0%B8%8A%E0%B8%A3%E0%B8%B2%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87-%E0%B8%AD%E0%B8%AB%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B8%A2%E0%B8%AD%E0%B8%94-%E0%B8%88%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87-107904924436991?openExternalBrowser=1"
-                  }
-                ]
-              },
-              {
-                thumbnailImageUrl: "https://www.i-pic.info/i/UQm6111429.jpg",
-                imageBackgroundColor: "#030600",
-                title: "\t\t เรือนจำจังหวัดตรัง",
-                text: " โทรศัพท์ 075582336\n ที่อยู่ 133 ม. 11 ตำบลโคกหล่อ อ.เมืองตรัง",
-               
-                actions: [
-                    {
-                    type: "uri",
-                    label: "รายละเอียดเพิ่มเติม",
-                   uri: "https://www.facebook.com/%E0%B9%80%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%99%E0%B8%88%E0%B8%B3%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87-100531712073093?openExternalBrowser=1"
-          }
-                ]
-              },
-          {
-                thumbnailImageUrl: "https://www.i-pic.info/i/7BSx111428.jpg",
-                imageBackgroundColor: "#ffffff",
-                title: "\t\t สถานพินิจและคุ้มครองเด็กและเยาวชน",
-                text: " โทรศัพท์ 075571439\n ที่อยู่ 116 ม.3 ต.ทุ่งค่าย อ.ย่านตาขาว",
-               
-                actions: [
-                   {
-                    type: "uri",
-                    label: "รายละเอียดเพิ่มเติม",
-                   uri: "https://shorturl.at/hxD17?openExternalBrowser=1"
-              }
-            ]
-            }
-               
-            ]
-          }
+  "type": "bubble",
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/86hK111426.png",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "กาชาดจังหวัดตรัง\n โทร 075501095 \n ที่อยู่ 8,ถ.รื่นรมย์\n ต.ทับเที่ยง",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://www.facebook.com/pg/trangrc/posts?openExternalBrowser=11"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+}, {
+  "type": "bubble", //บ้านพักคนชรา
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/Kmq9111425.jpg",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "สถานบ้านพักคนชรา\n โทร 075574679\n ที่อยู่ 113 ม.2 ต.ปากแจ่ม อ.ห้วยยอด",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://www.facebook.com/%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99%E0%B8%AA%E0%B8%87%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B2%E0%B8%B0%E0%B8%AB%E0%B9%8C%E0%B8%84%E0%B8%99%E0%B8%8A%E0%B8%A3%E0%B8%B2%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87-%E0%B8%AD%E0%B8%AB%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B8%A2%E0%B8%AD%E0%B8%94-%E0%B8%88%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87-107904924436991?openExternalBrowser=1",
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+ },
+ {
+  "type": "bubble",
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/UQm6111429.jpg",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "เรือนจำจังหวัดตรัง\n โทร 075582336 \n ที่อยู่ 133 ม. 11 ตำบลโคกหล่อ อ.เมืองตรัง",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://linecorp.com"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+ },
+ {
+  "type": "bubble", //สถานพินิจ
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/7BSx111428.jpg",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "สถานพินิจและคุ้มครองเด็กและเยาวชน\n โทร 075571439\n ที่อยู่ 116 ม.3 ต.ทุ่งค่าย อ.ย่านตาขาว",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },"footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://shorturl.at/hxD17?openExternalBrowser=1"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+ }
+  ]
+}
 }
 ];
 
