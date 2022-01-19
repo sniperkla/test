4eeda1dfc20fb55da69433ceb7dd703e8f719cf0
Mywork <mywork@Mys-MacBook-Pro.local>
2022-01-20 05:11:52 +0700

    sss

--- index.js
+++ index.js
@@ -710,7 +710,6 @@ const food = [
   }
  }
 
-
   ]
 }
 }
@@ -719,6 +718,158 @@ const food = [
 
 
 const money = [{
+"type" : "flex",
+    "altText" : "รายละเอียด", 
+    "contents" :{
+  "type": "carousel",
+  "contents": [
+ {
+  "type": "bubble",
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/86hK111426.png",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "กาชาดจังหวัดตรัง\n โทร 075501095 \n ที่อยู่ 8,ถ.รื่นรมย์\n ต.ทับเที่ยง",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://www.facebook.com/pg/trangrc/posts?openExternalBrowser=11"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+}, {
+  "type": "bubble", //หน่วยกู้ภัย มูลนิธิกุศลสถานตรั
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/HrDI111431.jpg",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "หน่วยกู้ภัย มูลนิธิกุศลสถานตรัง\n โทร 075215554 \n ที่อยู่ 127 ถ.โรงเรียน ต.ทับเที่ยง อ.เมืองตรัง",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://www.facebook.com/KUSOLSATHANTRANG?openExternalBrowser=1"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+},
+ {
+  "type": "bubble", //สถานพินิจ
+  "direction": "ltr",
+  "hero": {
+    "type": "image",
+    "url": "https://www.i-pic.info/i/7BSx111428.jpg",
+    "align": "center",
+    "size": "full",
+    "aspectRatio": "1:1",
+    "backgroundColor": "#FFFFFFFF"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "สถานพินิจและคุ้มครองเด็กและเยาวชน\n โทร 075571439\n ที่อยู่ 116 ม.3 ต.ทุ่งค่าย อ.ย่านตาขาว",
+        "weight": "regular",
+        "size": "md",
+        "color": "#000000FF",
+        "align": "center",
+        "wrap": true,
+        "contents": []
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "รายละเอียด",
+          "uri": "https://shorturl.at/hxD17?openExternalBrowser=1"
+        },
+        "color": "#008CC3FF",
+        "style": "primary",
+        "gravity": "center"
+      }
+    ]
+  }
+ }
+
+
+  ]
+}
+}
+];
+
+      
+
+
+
+
+[{
           type: "template",
           altText: "รายละเอียด",
           template: {
