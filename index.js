f8bd9a614f25f9acd3c6cf311f48f7db85f26cca
Mywork <mywork@Mys-MacBook-Pro.local>
2022-01-20 05:22:40 +0700

    sss

--- index.js
+++ index.js
@@ -814,11 +814,11 @@ const money = [
   }
 },
  {
-  "type": "bubble", //สถานพินิจ
+  "type": "bubble", //มูลนิธิสว่างภักดีตรังธรรมสถาน
   "direction": "ltr",
   "hero": {
     "type": "image",
-    "url": "https://www.i-pic.info/i/7BSx111428.jpg",
+    "url": "https://www.i-pic.info/i/dNKI111430.jpg",
     "align": "center",
     "size": "full",
     "aspectRatio": "1:1",
@@ -830,7 +830,7 @@ const money = [
     "contents": [
       {
         "type": "text",
-        "text": "สถานพินิจและคุ้มครองเด็กและเยาวชน\n โทร 075571439\n ที่อยู่ 116 ม.3 ต.ทุ่งค่าย อ.ย่านตาขาว",
+        "text": "มูลนิธิสว่างภักดีตรังธรรมสถาน\n โทร 075820328 \n ที่อยู่ 229/23 ต. บางรัก อ.เมืองตรัง",
         "weight": "regular",
         "size": "md",
         "color": "#000000FF",
@@ -849,7 +849,7 @@ const money = [
         "action": {
           "type": "uri",
           "label": "รายละเอียด",
-          "uri": "https://shorturl.at/hxD17?openExternalBrowser=1"
+          "uri": "https://www.facebook.com/%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%99%E0%B8%B4%E0%B8%98%E0%B8%B4%E0%B8%AA%E0%B8%A7%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%A0%E0%B8%B1%E0%B8%81%E0%B8%94%E0%B8%B5%E0%B8%95%E0%B8%A3%E0%B8%B1%E0%B8%87%E0%B8%98%E0%B8%A3%E0%B8%A3%E0%B8%A1%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99-102586631228919?openExternalBrowser=1",
         },
         "color": "#008CC3FF",
         "style": "primary",
@@ -857,7 +857,7 @@ const money = [
       }
     ]
   }
- },
+},
  {
   "type": "bubble", //สถานพินิจ
   "direction": "ltr",
@@ -902,15 +902,8 @@ const money = [
     ]
   }
  }
-
-
-
-
-
-
   ]
 }
-
 }
 ];
 
