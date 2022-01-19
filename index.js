3da995e016c4a1345866e062f38e3520d9d66696
Mywork <mywork@Mys-MacBook-Pro.local>
2022-01-20 04:41:56 +0700

    sss

--- index.js
+++ index.js
@@ -663,51 +663,6 @@ const food = [
       }
     ]
   }
- },
- {
-  "type": "bubble", //สถานพินิจ
-  "direction": "ltr",
-  "hero": {
-    "type": "image",
-    "url": "https://www.i-pic.info/i/7BSx111428.jpg",
-    "align": "center",
-    "size": "full",
-    "aspectRatio": "1:1",
-    "backgroundColor": "#FFFFFFFF"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "สถานพินิจและคุ้มครองเด็กและเยาวชน\n โทร 075571439\n ที่อยู่ 116 ม.3 ต.ทุ่งค่าย อ.ย่านตาขาว",
-        "weight": "regular",
-        "size": "md",
-        "color": "#000000FF",
-        "align": "center",
-        "wrap": true,
-        "contents": []
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "horizontal",
-    "contents": [
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "รายละเอียด",
-          "uri": "https://shorturl.at/hxD17?openExternalBrowser=1"
-        },
-        "color": "#008CC3FF",
-        "style": "primary",
-        "gravity": "center"
-      }
-    ]
-  }
  }
 
 
